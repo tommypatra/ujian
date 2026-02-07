845c1942bf08309434904f85cfcb328e0a0139a3
tommypatra <tommyirawan.patra@gmail.com>
2026-02-07 19:23:32 +0800

    filter input jabatan pengelola

--- app/requests/PengelolaSeleksiRequest.js
+++ app/requests/PengelolaSeleksiRequest.js
@@ -1,12 +1,13 @@
 // requests/PengelolaSeleksiRequest.js
 const Joi = require('joi');
+const jabatan_input = ['panitia', 'pembuat-soal', 'pengawas'];
 
 class PengelolaSeleksiRequest {
 
     static store(data) {
         return Joi.object({
             user_id: Joi.number().integer().positive().required(),
-            jabatan: Joi.string().min(3).required(),        
+            jabatan: Joi.string().valid(...jabatan_input).required()
         }).validate(data, {
             abortEarly: false,
             stripUnknown: true,
@@ -16,7 +17,7 @@ class PengelolaSeleksiRequest {
     static update(data) {
         return Joi.object({
             user_id: Joi.number().integer().positive().optional(),
-            jabatan: Joi.string().min(3).optional(),        
+            jabatan: Joi.string().valid(...jabatan_input).optional()
         })
         .min(1)
         .validate(data, {
